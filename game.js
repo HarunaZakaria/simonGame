514a9431df3482ef3eff2e7c49e5ba27e918b766
Sir Harunzy <Harunzy55@gmail.com>
2025-03-12 19:40:07 +0530

    add all the files

--- game.js
+++ game.js
@@ -7,9 +7,8 @@ function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     return randomNumber;
 }
-
 let randomChosenColour = buttonColours[nextSequence()];
 //add random color chosen to game pattern
 gamePattern.push(randomChosenColour);
 
-alert(gamePattern);
+//alert(gamePattern);
