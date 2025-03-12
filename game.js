f84c66e2d5b9ccbe2ff57ae8b05afbdedf60a84c
Sir Harunzy <Harunzy55@gmail.com>
2025-03-12 19:38:59 +0530

    add the initial functions for the game

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-alert("This is a Simon Game!");
+let gamePattern = [];
+//buttons colors
+let buttonColours = ["red", "blue", "green", "yellow"]
+
+// Next Sequence function
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    return randomNumber;
+}
+
+let randomChosenColour = buttonColours[nextSequence()];
+//add random color chosen to game pattern
+gamePattern.push(randomChosenColour);
+
+alert(gamePattern);
